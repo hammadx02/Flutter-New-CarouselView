50b7f1a0f95fed468f7ac52a8c7c32f226a4f44e
Hammad Ali <hammadaali02@gmail.com>
2024-08-08 21:32:18 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/new_carousel_view/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_carousel_view
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:new_carousel_view/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'New CarouselView',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
