9bde74b3ff3247e7f518aaf3de2295bf9e7d931d
Hammad Ali <hammadaali02@gmail.com>
2024-08-08 21:51:36 +0500

    completed

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,13 +7,37 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
         title: const Text(
           'New Carousel View',
           style: TextStyle(
             fontWeight: FontWeight.bold,
             letterSpacing: 2,
+            color: Colors.white,
           ),
         ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 240,
+            child: CarouselView(
+              itemExtent: MediaQuery.of(context).size.width - 32,
+              children: List.generate(10, (int index) {
+                return Container(
+                  color: Colors.grey,
+                  child: Image.network(
+                    'https://picsum.photos/400?random=$index',
+                    fit: BoxFit.cover,
+                  ),
+                );
+              }),
+            ),
+          ),
+        ],
       ),
     );
   }
