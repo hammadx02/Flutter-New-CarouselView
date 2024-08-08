d31a06fc640464664d84992662f08f7553a81655
Hammad Ali <hammadaali02@gmail.com>
2024-08-08 21:35:23 +0500

    updated

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,16 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'New Carousel View',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+          ),
+        ),
+      ),
+    );
   }
 }
